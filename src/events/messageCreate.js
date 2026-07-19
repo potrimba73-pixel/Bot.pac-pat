1399dffca96fb976191ba73c61bb8eb21de3b17e
potrimba73-pixel <potrimba73@gmail.com>
2026-07-19 19:40:30 +0100

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,13 +1,9 @@
-import { logExternalMessageCreate } from "../services/externalLogs.js";
+import { logExternalMessageDelete } from "../services/externalLogs.js";
 
 export async function handleMessageCreate(message, client) {
   if (message.author.bot) return;
   if (!message.guild) return;
 
-  // Log externo
-  try {
-    await logExternalMessageCreate(message);
-  } catch (e) {
-    // Silencioso
-  }
+  // Log externo de mensagens criadas nao esta implementado
+  // Podes adicionar logExternalMessageCreate no externalLogs.js se quiseres
 }
