abfe3ccc71709643b8bfe8297c9690e3781596a6
arte-trimbauser <arteompotrimba71@gmail.com>
2026-09-13 03:56:05 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -61,9 +61,9 @@ export const CONFIG = {
   TRUCKY_COMPANY_ID: process.env.TRUCKY_COMPANY_ID,
 
   // === IMAGENS (jsDelivr CDN - permanentes) ===
-  IMAGEM_GERAL: "https://ibb.co/rGWZVcPG",
-  IMAGEM_RECRUTAMENTO: "https://ibb.co/XrpS5QXF",
-  IMAGEM_REGRAS: "https://ibb.co/spzXgzhx",
+  IMAGEM_GERAL: "https://i.ibb.co/6Rd4k1hR/Tickets-da-Portugal-Alfa-Community.webp",
+  IMAGEM_RECRUTAMENTO: "https://i.ibb.co/6Rd4k1hR/Tickets-da-Portugal-Alfa-Community.webp",
+  IMAGEM_REGRAS: "https://i.ibb.co/KcS85SY4/Regras-da-Portugal-Alfa-Community.webp",
 
   // === CORES ===
   COR_PRINCIPAL: 0x262af1,
